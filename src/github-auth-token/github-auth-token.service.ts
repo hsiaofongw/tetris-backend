a0046476c3109bf5207e629eab04f3ad5e2c42e9
hsiaofongw <hsiaofong.w@gmail.com>
2021-10-03 18:46:38 +0800

    feat(oauth): 更新clientSecret获取逻辑

--- src/github-auth-token/github-auth-token.service.ts
+++ src/github-auth-token/github-auth-token.service.ts
@@ -3,6 +3,7 @@ import { AxiosResponse } from '@nestjs/common/node_modules/axios';
 import axios from 'axios';
 import { TokenInfo } from 'src/token/interfaces';
 
+/** GitHub OAuth 服务器与 token 一并返回的东西的类型 */
 type ServerReturn = {
   access_token: string;
   scope: string;
@@ -12,10 +13,13 @@ type ServerReturn = {
   refresh_token_expires_in?: number;
 };
 
+/** 此服务负责向 GitHub OAuth 服务器请求 Token 以为接下来进一步获取被授权的资源做准备 */
 @Injectable()
 export class GithubAuthTokenService {
-  async getToken(authorizationCode: string) {
-    const clientSecret = '';
+  async getToken(authorizationCode: string): Promise<TokenInfo> {
+    const clientSecret =
+      process.env['OAUTH_CLIENT_SECRET'] ?? 'noClientSecretFound';
+
     const clientId = 'Iv1.904dfc227b774cc7';
     const payload = {
       client_id: clientId,
@@ -31,8 +35,6 @@ export class GithubAuthTokenService {
         headers: { Accept: 'application/json' },
       },
     );
-
-    console.log({ data });
 
     return {
       accessToken: data.access_token,
