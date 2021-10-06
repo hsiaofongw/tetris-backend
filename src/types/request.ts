56c6ed35fc105a4259092bc44515082d63d70ac0
hsiaofongw <hsiaofong.w@gmail.com>
2021-10-06 17:17:59 +0800

    feat: 能够获取用户名

---
+++ src/types/request.ts
@@ -0,0 +1,11 @@
+export type ErrorInfo = {
+  statusCode?: number;
+  message?: string;
+  errorCode?: string;
+  errorUri?: string;
+};
+
+export type QueryResult<T> = {
+  error?: ErrorInfo;
+  result: T;
+};
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,18 +1,47 @@
-import { Controller, Get, Request, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  InternalServerErrorException,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { ApiBearerAuth } from '@nestjs/swagger';
-import { ILoginInfo, LoginService } from 'src/login/login.service';
+import axios from 'axios';
+import { ILoginInfo } from 'src/login/login.service';
+import { UserProfileQueryResult } from './interfaces';
 
 export type UserInfo = { userId: number; loginInfo: ILoginInfo };
 
 @Controller('user')
 export class UserController {
-  constructor(private loginService: LoginService) {}
-
   @ApiBearerAuth()
   @UseGuards(AuthGuard('jwt'))
   @Get()
   getUserInfo(@Request() req) {
-    return req.user;
+    const loginInfo = req.user as ILoginInfo;
+    if (!loginInfo) {
+      throw new InternalServerErrorException();
+    }
+
+    const token = loginInfo.oauthTokenInfo.accessToken;
+
+    return axios
+      .get<any, any>('https://api.github.com/user', {
+        headers: {
+          Authorization: 'token ' + token,
+          Accept: 'application/json',
+        },
+        proxy: {
+          host: '127.0.0.1',
+          port: 7890,
+        },
+        responseType: 'json',
+      })
+      .then((response) => {
+        return {
+          username: response.data?.login ?? '',
+        } as UserProfileQueryResult['result'];
+      });
   }
 }
