e6df228b0b55ad514f9417949a8fbb88cab8af31
hsiaofongw <hsiaofong.w@gmail.com>
2021-10-07 17:40:46 +0800

    feat: 更新了接口地址前缀

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  app.setGlobalPrefix('api/v1');
   app.enableCors();
   app.useGlobalFilters(new HttpExceptionFilter());
 
